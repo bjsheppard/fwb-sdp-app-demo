c47729a02cbb1cdcf502851ee292708145f176ec
Brandon S <sheppard_brandon@bah.com>
2019-12-03 16:01:09 -0500

    removed not yet implemented docker deploy to steps and added back sdp docker args

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,7 +3,7 @@ libraries{
     images{
       registry = "http://0.0.0.0:5000" // registry url
       cred = "sdp-docker-registry"// jenkins cred id to authenticate
-      // docker_args = "--network=try-it-out_sdp"  // docker runtime args
+      docker_args = "--network=try-it-out_sdp"  // docker runtime args
     }
   }
   github_enterprise
